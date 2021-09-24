49622d04d41acc600bab01b3a24334428b142c4e
TheVoid-0 <marcodnmoura@gmail.com>
2021-09-24 03:37:35 -0300

    Add FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,26 @@
 // MPU sensor
 #include <Wire.h>
 #include <SPI.h>
+#include "config/Mqtt_config.h"
+#include "config/Wifi_config.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
 
 // Bluetooth App
+/**
+ * OBS: A biblioteca BlynkSimpleEsp32_BLE.h Possui variáveis erroneamente declaradas no arquivo de HEADER (*.h)
+ * isso causa erros ao incluir a lib em vários arquivos, o que é necessário para incluir seus tipos e tornar seus
+ * simbolos acessíveis, para resolver isso, é necessário retirar a instanciação do arquivo .h, expondo a variável
+ * com 'extern' e então declarar ela onde deseja usar
+ * EX: 
+        static BlynkTransportEsp32_BLE _blynkTransportBLE;
+        extern BlynkEsp32_BLE Blynk; <- Alterado essa linha adicionando o 'extern' e retirando a construção (_blynkTransportBLE)
+ * TODO: Verificar para adicionar uma biblioteca personalizada que inclui a BlynkSimpleEsp32_BLE.h e define suas variáveis em um .cpp
+ separado para então incluir nos arquivos do projeto a lib personalizada
+ */
 #include <BlynkSimpleEsp32_BLE.h>
-BlynkEsp32_BLE Blynk(_blynkTransportBLE);
+BlynkEsp32_BLE Blynk(_blynkTransportBLE); // Construindo a variável no .cpp já que a expomos usando o extern no .h da lib
 // #include <BLEDevice.h>
 // #include <BLEServer.h>
 
@@ -21,15 +34,14 @@ BlynkEsp32_BLE Blynk(_blynkTransportBLE);
 #include <WifiManager.h>
 #include <MqttManager.h>
 #include <I2CManager.h>
-#include "config/Mqtt_config.h"
-#include "config/Wifi_config.h"
 #include "service/sensor-service/SensorService.h"
 #include "service/bluetooth-service/BluetoothService.h"
 
 // MAIN DECLARED FUNCTIONS
-void init_MPU();
 void get_accelerometer_and_gyroscope_data();
 void mqtt_callback(char *topic, byte *payload, unsigned int length);
+void core1_task(void *parameters);
+void core2_task(void *parameters);
 
 // MAIN DECLARED VARIABLES
 bool IS_EMULATOR = true;
@@ -61,11 +73,6 @@ void setup()
   pinMode(39, OUTPUT);
   pinMode(2, OUTPUT);
 
-  if (!IS_EMULATOR)
-  {
-    init_MPU();
-  }
-
   wifi_manager = new WifiManager(WIFI_SSID, WIFI_PASSWORD);
 
   mqtt = new MqttManager(WIFI_SSID, WIFI_PASSWORD, BROKER_MQTT, BROKER_MQTT_PORT, ID_MQTT, TOKEN_MQTT, mqtt_callback);
@@ -75,26 +82,65 @@ void setup()
   bluetooth_service = new BluetoothService(mqtt);
 
   Wire.begin();
+  Serial.println("Creating tasks");
+  xTaskCreatePinnedToCore(
+      core1_task,   /* Function to implement the task */
+      "core1_task", /* Name of the task */
+      10000,        /* Stack size in words */
+      NULL,         /* Task input parameter */
+      0,            /* Priority of the task */
+      NULL,         /* Task handle. */
+      0);           /* Core where the task should run */
+
+  xTaskCreatePinnedToCore(
+      core2_task,   /* Function to implement the task */
+      "core2_task", /* Name of the task */
+      10000,        /* Stack size in words */
+      NULL,         /* Task input parameter */
+      2,            /* Priority of the task precisa de uma prioridade maior que 0 para disputar com o loop do arduino*/
+      NULL,         /* Task handle. */
+      1);           /* Core where the task should run */
+
+  Serial.println("Done!");
 }
 
 void loop()
 {
-  if (!IS_EMULATOR)
-  {
-    i2c_manager.find_I2C_Address();
-  }
-
-  wifi_manager->check_connection();
-
-  sensor_service->publishData();
-
-  /* keep-alive da comunicação com broker MQTT */
-  mqtt->check_connection();
-
-  bluetooth_service->run();
-  delay(1000);
+  Serial.print("Loop no core:"); // API do arduino maldita roda esse loop independentemente de ser util ou não
+  Serial.println(xPortGetCoreID());
+  vTaskDelete(NULL);  // Adeus loop do arduino :)
 }
 
+void core1_task(void *parameters)
+{
+  for (;;)
+  {
+    bluetooth_service->run();
+    vTaskDelay(10); // Deixa o watchdog do freeRTOS fazer o necessário nesse core, caso contrário o loop inifinito bloqueará a execução no núcleo e causará um erro
+  }
+}
+void core2_task(void *parameters)
+{
+  for (;;)
+  {
+    Serial.println("core2_task");
+    if (!IS_EMULATOR)
+    {
+      i2c_manager.find_I2C_Address();
+    }
+
+    wifi_manager->check_connection();
+
+    sensor_service->publishData();
+
+    /* keep-alive da comunicação com broker MQTT */
+    mqtt->check_connection();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+/**
+ * Função chamada pelo App BLYNK ao apertar o botão designado como V1
+ */
 BLYNK_WRITE(V1)
 {
   Serial.println("Bluetooth command");
